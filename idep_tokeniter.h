ef8823e8f38d14b12702693520c8757ffee05bdd
Thiago Farina <tfarina@chromium.org>
2012-08-15 23:03:13 -0300

    Move private section of TokenIter to the end of class.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- idep_tokeniter.h
+++ idep_tokeniter.h
@@ -10,33 +10,33 @@ using namespace std;
 
 class idep_TokenIter_i;
 class idep_TokenIter {
-    idep_TokenIter_i *d_this;
-
-  private:
-    idep_TokenIter(const idep_TokenIter&);              // not implemented
-    idep_TokenIter& operator=(const idep_TokenIter&);   // not implemented
-
   public:
     // CREATORS
     idep_TokenIter(std::istream& in);
-        // Create a token iterator for the specified stream.  A "token" is 
-        // either a newline ('\n') or a "word" consisting of a contiguous 
+        // Create a token iterator for the specified stream.  A "token" is
+        // either a newline ('\n') or a "word" consisting of a contiguous
         // sequence of non-white-space characters.  The stream object must
         // continue to exist while the iterator is in use.
 
     ~idep_TokenIter();
 
     void operator++();
-        // Advance to next token (i.e., "word" or newline).  The behavior is 
-        // undefined if the iteration state is not valid. 
+        // Advance to next token (i.e., "word" or newline).  The behavior is
+        // undefined if the iteration state is not valid.
 
     // ACCESSORS
     operator const void *() const;
         // Return non-zero if current token is valid; else 0.
 
     const char *operator()() const;
-        // Return the current token (i.e., "word" or newline).  The behavior 
+        // Return the current token (i.e., "word" or newline).  The behavior
         // is undefined if the iteration state is not valid.
+
+ private:
+  idep_TokenIter_i *d_this;
+
+  idep_TokenIter(const idep_TokenIter&);
+  idep_TokenIter& operator=(const idep_TokenIter&);
 };
 
 #endif  // IDEP_TOKENITER_H_
