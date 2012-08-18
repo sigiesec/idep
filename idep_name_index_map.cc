fdeddca9ff2015bd08631b601e2245b71b74a530
Thiago Farina <tfarina@chromium.org>
2012-08-18 19:47:30 -0300

    Rename idep_NameIndexMapLink to just NameIndexMapLink.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- idep_name_index_map.cc
+++ idep_name_index_map.cc
@@ -19,25 +19,25 @@ static unsigned hash(register const char* name) {
     return sum; // unsigned ensures positive value for use with (%) operator.
 }
 
-struct idep_NameIndexMapLink {
+struct NameIndexMapLink {
  const char *d_name_p;                       // name
  int d_index;                                // index of name
- idep_NameIndexMapLink *d_next_p;            // pointer to next link
+ NameIndexMapLink *d_next_p;            // pointer to next link
 
- idep_NameIndexMapLink(const char* name,
+ NameIndexMapLink(const char* name,
                        int index,
-                       idep_NameIndexMapLink* d_next_p);
+                       NameIndexMapLink* d_next_p);
 };
 
-idep_NameIndexMapLink::idep_NameIndexMapLink(const char* name,
+NameIndexMapLink::NameIndexMapLink(const char* name,
                                              int index,
-                                             idep_NameIndexMapLink *next)
+                                             NameIndexMapLink *next)
     : d_name_p(name),
       d_index(index),
       d_next_p(next) {
 }
 
-static const idep_NameIndexMapLink *find(const idep_NameIndexMapLink *p,
+static const NameIndexMapLink *find(const NameIndexMapLink *p,
                                          const char *name)
 {
     while (p && 0 != strcmp(p->d_name_p, name)) {
@@ -48,7 +48,7 @@ static const idep_NameIndexMapLink *find(const idep_NameIndexMapLink *p,
 
 struct NameIndexMapImpl {
     idep::NameArray d_array;                     // array of names
-    idep_NameIndexMapLink **d_table_p;          // hash table of names
+    NameIndexMapLink **d_table_p;          // hash table of names
     int d_tableSize;                            // size of hash table
 
     NameIndexMapImpl(int size);
@@ -56,10 +56,10 @@ struct NameIndexMapImpl {
 
     ~NameIndexMapImpl();
 
-    idep_NameIndexMapLink *& findSlot(const char *name);
+    NameIndexMapLink *& findSlot(const char *name);
         // find the appropriate slot for this name
 
-    int insert(idep_NameIndexMapLink *& slot, const char *name);
+    int insert(NameIndexMapLink *& slot, const char *name);
         // insert name into specified slot
 };
 
@@ -67,7 +67,7 @@ NameIndexMapImpl::NameIndexMapImpl(int size)
 : d_array(size)
 , d_tableSize(size > 0 ? size : DEFAULT_TABLE_SIZE)
 {
-    d_table_p = new idep_NameIndexMapLink *[d_tableSize];
+    d_table_p = new NameIndexMapLink *[d_tableSize];
     assert(d_table_p);
     memset(d_table_p, 0, d_tableSize * sizeof *d_table_p);
 }
@@ -75,9 +75,9 @@ NameIndexMapImpl::NameIndexMapImpl(int size)
 NameIndexMapImpl::~NameIndexMapImpl()
 {
     for (int i = 0; i < d_tableSize; ++i) {
-        idep_NameIndexMapLink *p = d_table_p[i];
+        NameIndexMapLink *p = d_table_p[i];
         while (p) {
-        idep_NameIndexMapLink *q = p;
+        NameIndexMapLink *q = p;
             p = p->d_next_p;
             delete q;
         }
@@ -85,17 +85,17 @@ NameIndexMapImpl::~NameIndexMapImpl()
     delete [] d_table_p;
 }
 
-idep_NameIndexMapLink *& NameIndexMapImpl::findSlot(const char *name)
+NameIndexMapLink *& NameIndexMapImpl::findSlot(const char *name)
 {
     int index = hash(name) % d_tableSize;
     assert(index >= 0 && index < d_tableSize);
     return d_table_p[index];
 }
 
-int NameIndexMapImpl::insert(idep_NameIndexMapLink *& slot, const char *nm)
+int NameIndexMapImpl::insert(NameIndexMapLink *& slot, const char *nm)
 {
     int index = d_array.Append(nm); // index is into a managed string array
-    slot = new idep_NameIndexMapLink(d_array[index], index, slot);
+    slot = new NameIndexMapLink(d_array[index], index, slot);
     return index;
 }
 
@@ -113,14 +113,14 @@ idep_NameIndexMap::~idep_NameIndexMap()
 
 int idep_NameIndexMap::add(const char* name)
 {
-    idep_NameIndexMapLink *& slot = impl_->findSlot(name);
+    NameIndexMapLink *& slot = impl_->findSlot(name);
     return find(slot, name) ? BAD_INDEX : impl_->insert(slot, name);
 }
 
 int idep_NameIndexMap::entry(const char* name)
 {
-    idep_NameIndexMapLink *& slot = impl_->findSlot(name);
-    const idep_NameIndexMapLink *link = find(slot, name);
+    NameIndexMapLink *& slot = impl_->findSlot(name);
+    const NameIndexMapLink *link = find(slot, name);
     return link ? link->d_index : impl_->insert(slot, name);
 }
 
@@ -136,8 +136,8 @@ int idep_NameIndexMap::length() const
 
 int idep_NameIndexMap::lookup(const char *name) const
 {
-    idep_NameIndexMapLink *& slot = impl_->findSlot(name);
-    const idep_NameIndexMapLink *link = find(slot, name);
+    NameIndexMapLink *& slot = impl_->findSlot(name);
+    const NameIndexMapLink *link = find(slot, name);
     return link ? link->d_index : BAD_INDEX;
 }
 
