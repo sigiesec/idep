8dadb701dbae7d9009ce0fd10737e2b8da430236
Thiago Farina <tfarina@chromium.org>
2012-08-19 20:12:46 -0300

    Clean up idep_binary_relation.h header file.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- idep_binary_relation.cc
+++ idep_binary_relation.cc
@@ -1,7 +1,7 @@
 #include "idep_binary_relation.h"
 
 #include <assert.h>
-#include <memory.h>     // memcpy() memset() memcmp()
+#include <memory.h>
 
 #include <iostream>
 
@@ -20,7 +20,7 @@
 
 enum { START_SIZE = 1, GROW_FACTOR = 2 };
 
-static void clean(char **p)  {
+static void clean(char** p)  {
   /* crash, bt:
 #0  0x00002aaaab08011d in raise () from /lib/libc.so.6
 #1  0x00002aaaab08184e in abort () from /lib/libc.so.6
@@ -37,7 +37,7 @@ static void clean(char **p)  {
     delete [] p;                // delete single block
 }
 
-static char **alloc(int size) {
+static char** alloc(int size) {
     register int s = size;
     char **rel = new char *[s];
     register char *p = new char[s * s];
@@ -47,35 +47,30 @@ static char **alloc(int size) {
     return rel;
 }
 
-static void clear(char *const *rel, int size) 
-{
+static void clear(char *const *rel, int size) {
     memset(*rel, 0, size * size);
 }
 
-static void copy(char **left, const char *const *right, int size)
-{
+static void copy(char **left, const char *const *right, int size) {
     memcpy(*left, *right, size * size);
 }
 
 namespace idep {
 
-void BinaryRelation::grow()
-{
+void BinaryRelation::grow() {
     int newSize = d_size * GROW_FACTOR;
     char **tmp = d_rel_p;
     d_rel_p = alloc(newSize);
     clear(d_rel_p, newSize);
 
-    for (int i = 0; i < d_size; ++i) {
+    for (int i = 0; i < d_size; ++i)
         memcpy(d_rel_p[i], tmp[i], d_size);
-    }
 
     d_size = newSize;
     clean(tmp);
 }
 
-void BinaryRelation::compress()
-{
+void BinaryRelation::compress() {
     if (d_size > d_length && d_length > 0) {
         d_size = d_length;
         char **tmp = d_rel_p;
@@ -88,37 +83,32 @@ void BinaryRelation::compress()
     }
 }
 
-                // -*-*-*- public functions -*-*-*-
-
-BinaryRelation::BinaryRelation(int initialEntries, int max_entries_hint) 
-: d_size(max_entries_hint > 0 ? max_entries_hint : START_SIZE) 
-, d_length(initialEntries > 0 ? initialEntries : 0)
-{
-    if (d_size < d_length) {
+BinaryRelation::BinaryRelation(int initial_entries, int max_entries_hint)
+    : d_size(max_entries_hint > 0 ? max_entries_hint : START_SIZE),
+      d_length(initial_entries > 0 ? initial_entries : 0) {
+    if (d_size < d_length)
         d_size = d_length;
-    }
+
     d_rel_p = alloc(d_size);
     clear(d_rel_p, d_size);
 }
 
-BinaryRelation::BinaryRelation(const BinaryRelation& rel) 
-: d_size(rel.d_size)
-, d_length(rel.d_length)
-, d_rel_p(alloc(rel.d_size))
-{ 
-  // TODO 
+BinaryRelation::BinaryRelation(const BinaryRelation& rel)
+    : d_size(rel.d_size),
+      d_length(rel.d_length),
+      d_rel_p(alloc(rel.d_size)) {
+  // TODO
   copy(d_rel_p, d_rel_p + d_size, rel.d_size);
 }
 
-BinaryRelation& BinaryRelation::operator=(const BinaryRelation& rel) 
-{  
+BinaryRelation& BinaryRelation::operator=(const BinaryRelation& rel) {
     if (&rel != this) {
-        if (d_size != rel.d_size) { 
+        if (d_size != rel.d_size) {
             clean(d_rel_p);
             d_size = rel.d_size;
             d_rel_p = alloc(d_size);
         }
-        //TODO
+        //TODO: There is a BUG HERE!
 	copy(d_rel_p, d_rel_p + d_size, rel.d_size);
 	//	copy(d_rel_p, rel.d_rel_p, d_size);
         d_length = rel.d_length;
@@ -126,48 +116,43 @@ BinaryRelation& BinaryRelation::operator=(const BinaryRelation& rel)
     return *this;
 }
 
-BinaryRelation::~BinaryRelation() 
-{
+BinaryRelation::~BinaryRelation() {
   clean(d_rel_p); // part of crash
 }
 
-int BinaryRelation::cmp(const BinaryRelation& rel) const
-{
+int BinaryRelation::cmp(const BinaryRelation& rel) const {
     enum { SAME = 0, DIFFERENT = 1 };
 
-    if (d_length != rel.d_length) {
+    if (d_length != rel.d_length)
         return DIFFERENT;
-    }
 
-    if (d_size == rel.d_size && 
-                            memcmp(*d_rel_p, *rel.d_rel_p, d_size * d_size)) {
+    if (d_size == rel.d_size &&
+        memcmp(*d_rel_p, *rel.d_rel_p, d_size * d_size)) {
         return DIFFERENT;
     }
 
     for (int i = 0; i < d_length; ++i) {
-        if (memcmp(d_rel_p[i], rel.d_rel_p[i], d_length)) {
+        if (memcmp(d_rel_p[i], rel.d_rel_p[i], d_length))
             return DIFFERENT;
-        }
     }
 
     return SAME;
 }
 
-void BinaryRelation::warshall(int bit) 
-{
+void BinaryRelation::warshall(int bit) {
     // DEFINITION: Transitive Closure
     //
     // Given: R is a relation (matrix) indicating transitions in 1 step.
-    // TransitiveClosure(R) = R + R^2 + R^3 + ... + R^N 
+    // TransitiveClosure(R) = R + R^2 + R^3 + ... + R^N
     //      where N is the number of rows and columns in the square matrix R.
     //
     // Warshall's algorithm to construct the transitive closure:
     // foreach index k
     //     foreach row i
     //         foreach column j
-    //             if (A[i][k] && A[k][j]) 
-    //                 A[i][j] = 1;      
-    // 
+    //             if (A[i][k] && A[k][j])
+    //                 A[i][j] = 1;
+    //
     // See Aho, Hopcroft, & Ullman, "Data Structures And Algorithms,"
     // Addison-Wesley, Reading MA, pp. 212-213.  Also see, Warshall, S. [1962].
     // "A theorem on Boolean matrices," Journal of the ACM, 9:1, pp. 11-12.
@@ -182,17 +167,17 @@ void BinaryRelation::warshall(int bit)
     for (int k = 0; k < s; ++k) {
         register char *row_k = d_rel_p[k];
         for (register char *row_r = d_rel_p[0]; row_r < top_row; row_r += s) {
-            if (!row_r[k]) { 
+            if (!row_r[k]) {
                 continue;                   // huge optimization
             }
-            if (d_rel_p[k] == row_r) {  
+            if (d_rel_p[k] == row_r) {
                 continue;                   // note: ignore self dependency
             }
             for (register int c = 0; c < s; ++c) {
                 if (row_k[c] && row_r[k]) { // note: both conditions are needed
                                             // in reverse (consider k == c).
                     if (c != k) {           // note: ignore self dependency
-                        row_r[c] = VALUE;   
+                        row_r[c] = VALUE;
                     }
                 }
             }
@@ -200,22 +185,18 @@ void BinaryRelation::warshall(int bit)
     }
 }
 
-void BinaryRelation::makeTransitive() 
-{
-    warshall(1);
+void BinaryRelation::makeTransitive() {
+  warshall(1);
 }
 
-void BinaryRelation::makeNonTransitive() 
-{
-    warshall(0);
-    // make non-reflexive too -- i.e., subtract the identity matrix.
-    for (int i = 0; i < Length(); ++i) {
-        d_rel_p[i][i] = 0;
-    }
+void BinaryRelation::makeNonTransitive() {
+  warshall(0);
+  // make non-reflexive too -- i.e., subtract the identity matrix.
+  for (int i = 0; i < Length(); ++i)
+    d_rel_p[i][i] = 0;
 }
 
-std::ostream& operator<<(std::ostream& o, const BinaryRelation& rel) 
-{
+std::ostream& operator<<(std::ostream& o, const BinaryRelation& rel) {
     int r, c;
     const int GAP_GRID = 10;
     const char *SP = rel.Length() < 30 ? " " : "";
@@ -225,7 +206,7 @@ std::ostream& operator<<(std::ostream& o, const BinaryRelation& rel)
         }
 
         for (c = 0; c < rel.Length(); ++c) {
-            if (c && 0 == c % GAP_GRID) { 
+            if (c && 0 == c % GAP_GRID) {
                 o << ' ' << SP;
             }
             o << (rel.get(r,c) ? '1' : '_') << SP;
--- idep_binary_relation.h
+++ idep_binary_relation.h
@@ -8,114 +8,102 @@ namespace idep {
 // This leaf component defines 1 class:
 // Square matrix of bits with transitive closure capability.
 class BinaryRelation {
-  public:
-    // CREATORS
-    BinaryRelation(int initialEntries = 0, int max_entries_hint = 0);
-        // Create a binary relation that can be extended as needed.
-        // By default, the initial number of entires in the relation
-        // is 0.  If the final number of entries is known and is not
-        // the same as initialEntries, that value may optionally be
-        // specified as the second argument (as a "hint").
+ public:
+  // Create a binary relation that can be extended as needed.
+  // By default, the initial number of entires in the relation
+  // is 0.  If the final number of entries is known and is not
+  // the same as initialEntries, that value may optionally be
+  // specified as the second argument (as a "hint").
+  BinaryRelation(int initial_entries = 0, int max_entries_hint = 0);
 
-    BinaryRelation(const BinaryRelation& rel);
-    ~BinaryRelation();
+  BinaryRelation(const BinaryRelation& rel);
+  ~BinaryRelation();
 
-    // MANIPULATORS
-    BinaryRelation& operator=(const BinaryRelation& rel);
+  BinaryRelation& operator=(const BinaryRelation& rel);
 
-    void set(int row, int col, int bit);
-        // Set the specified row/col of this relation to the specified 
-        // binary value.
+  // Set the specified row/col of this relation to the specified 
+  // binary value.
+  void set(int row, int col, int bit);
 
-    void set(int row, int col);
-        // Set the specified row/col of this relation to 1.
+  void set(int row, int col);
+  // Set the specified row/col of this relation to 1.
 
-    void clr(int row, int col);
-        // Set specified row/col of this relation to 0.
+  void clr(int row, int col);
+  // Set specified row/col of this relation to 0.
 
-    void makeTransitive();
-        // Apply Warshall's algorithm to this relation.  The result is the
-        // reflexive transitive closure of the original relation.
+  void makeTransitive();
+  // Apply Warshall's algorithm to this relation.  The result is the
+  // reflexive transitive closure of the original relation.
 
-    void makeNonTransitive();
-        // Remove all redundant relationships such that the transitive
-        // closure would be left unaffected.  In cases where there is
-        // a cyclic dependency, the solution is not unique.  To work 
-        // properly, the relation must already be fully transitive 
-        // (see makeTransitive).
+  void makeNonTransitive();
+  // Remove all redundant relationships such that the transitive
+  // closure would be left unaffected.  In cases where there is
+  // a cyclic dependency, the solution is not unique.  To work 
+  // properly, the relation must already be fully transitive 
+  // (see makeTransitive).
 
-    int appendEntry();
-        // Append an entry to this relation and return its integer index.
-        // The logical size is increased by 1 with all new entries 0'ed.
+  int appendEntry();
+  // Append an entry to this relation and return its integer index.
+  // The logical size is increased by 1 with all new entries 0'ed.
 
-    // ACCESSORS
-    int get(int row, int col) const;
-        // Get the boolean value at the specified row/col of this relation.
+  // ACCESSORS
+  int get(int row, int col) const;
+  // Get the boolean value at the specified row/col of this relation.
 
-    int cmp(const BinaryRelation& rel) const;
-        // Return 0 if and only if the specified relation has the same
-        // length and logical values as this relation.
+  int cmp(const BinaryRelation& rel) const;
+  // Return 0 if and only if the specified relation has the same
+  // length and logical values as this relation.
 
-    int Length() const;
-        // Return number of rows and columns in this relation.  The length
-        // represents the cardinality of the set on which this relation is
-        // defined.
+  // Return number of rows and columns in this relation.  The length
+  // represents the cardinality of the set on which this relation is
+  // defined.
+  int Length() const;
 
-  private:
-    void grow();
-      // Increase the physical size of this relation.
+ private:
+  // Increase the physical size of this relation.
+  void grow();
 
-    void compress();
-      // Make the physical size same as Logical size (unless that is 0).
+  // Make the physical size same as Logical size (unless that is 0).
+  void compress();
 
-    void warshall(int bit);
-      // Perform Warshall's algorithm either forward or backward. 
+  // Perform Warshall's algorithm either forward or backward. 
+  void warshall(int bit);
 
-    char **d_rel_p;     // array of pointers into a contiguous byte array
-    int d_size;         // physical size of array
-    int d_length;       // logical size of array
+  char **d_rel_p;     // array of pointers into a contiguous byte array
+  int d_size;         // physical size of array
+  int d_length;       // logical size of array
 };
 
+// Output this binary relation in row/column format with the upper left 
+// corner as the origin to the specified output stream (out).
 std::ostream& operator<<(std::ostream& out, const BinaryRelation& rel);
-   // Output this binary relation in row/column format with the upper left 
-   // corner as the origin to the specified output stream (out).
 
+// Return 1 if two relations are equal, and 0 otherwise (see cmp).
 int operator==(const BinaryRelation& left, const BinaryRelation& right);
-   // Return 1 if two relations are equal, and 0 otherwise (see cmp).
 
+// Return 1 if two relations are not equal, and 0 otherwise (see cmp).
 int operator!=(const BinaryRelation& left, const BinaryRelation& right);
-   // Return 1 if two relations are not equal, and 0 otherwise (see cmp).
 
-
-// #########################################################################
-// The following consists of inline function definitions for this component. 
-// #########################################################################
-
-inline int BinaryRelation::appendEntry() 
-{
+inline int BinaryRelation::appendEntry() {
     if (d_length >= d_size) {
         grow();
     }
     return d_length++;
 }
 
-inline void BinaryRelation::set(int row, int col, int bit) 
-{
+inline void BinaryRelation::set(int row, int col, int bit) {
     d_rel_p[row][col] = !!bit;
 }
 
-inline void BinaryRelation::set(int row, int col) 
-{
+inline void BinaryRelation::set(int row, int col) {
     d_rel_p[row][col] = 1;
 }
 
-inline void BinaryRelation::clr(int row, int col) 
-{
+inline void BinaryRelation::clr(int row, int col) {
     d_rel_p[row][col] = 0;
 }
 
-inline int BinaryRelation::get(int row, int col) const
-{
+inline int BinaryRelation::get(int row, int col) const {
     return d_rel_p[row][col];
 }
 
