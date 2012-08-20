0e73648307a27089b3db38879d6bc52ca8137cfc
Thiago Farina <tfarina@chromium.org>
2012-08-19 21:36:09 -0300

    Remove NL macro from cdep.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- cdep.cc
+++ cdep.cc
@@ -4,29 +4,26 @@
 
 // This file contains a main program to exercise the idep_compile_dep component.
 
-#define NL "\n"
-
 static const char* help() {
-return NL
-"cdep: Extract compile-time dependencies from a collection of files."        NL
-""                                                                           NL
-"  The following command line interface is supported:"                       NL
-""                                                                           NL
-"    cdep [-I<dir>] [-i<dirlist>] [-f<filelist>] [-x] <filename>*"           NL
-""                                                                           NL
-"      -I<dir>      Specify include directory to search."                    NL
-"      -i<dirlist>  Specify file containing a list of directories to search."NL
-"      -f<filelist> Specify file containing a list of files to process."     NL
-"      -x           Do _not_ check recursively for nested includes."         NL
-""                                                                           NL
-"    Each filename on the command line specifies a file to be considered for"NL
-"    processing.  Specifying no arguments indicates that the list of files"  NL
-"    is to come from standard input unless the -f option has been invoked."  NL
-""                                                                           NL
-"  TYPICAL USAGE:"                                                           NL
-""                                                                           NL
-"    cdep -iincludes *.[ch]"                                                 NL
-NL;
+return
+"\ncdep: Extract compile-time dependencies from a collection of files.\n"
+"\n"
+"  The following command line interface is supported:\n"
+"\n"
+"    cdep [-I<dir>] [-i<dirlist>] [-f<filelist>] [-x] <filename>*\n"
+"\n"
+"      -I<dir>      Specify include directory to search.\n"
+"      -i<dirlist>  Specify file containing a list of directories to search.\n"
+"      -f<filelist> Specify file containing a list of files to process.\n"
+"      -x           Do _not_ check recursively for nested includes.\n"
+"\n"
+"    Each filename on the command line specifies a file to be considered for\n"
+"    processing.  Specifying no arguments indicates that the list of files\n"
+"    is to come from standard input unless the -f option has been invoked.\n"
+"\n"
+"  TYPICAL USAGE:\n"
+"\n"
+"    cdep -iincludes *.[ch]\n\n";
 }
 
 static enum { IOERROR = -1, GOOD = 0 } s_status = GOOD;
