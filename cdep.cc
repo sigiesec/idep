fa66c33cdbad2d4d2018d0d8f7e6d68e520386ab
Thiago Farina <tfarina@chromium.org>
2012-09-16 21:09:08 -0300

    Use SkDebugf to print out the cdep usage.
    I'll clean up this later.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- cdep.cc
+++ cdep.cc
@@ -24,6 +24,20 @@ const char cdep_usage[] =
 "\n"
 "    cdep -iincludes *.[ch]\n\n";
 
+static const size_t kBufferSize = 2048;
+
+#include <stdarg.h>
+#include <stdio.h>
+
+void SkDebugf(const char format[], ...) {
+    char    buffer[kBufferSize + 1];
+    va_list args;
+    va_start(args, format);
+    vsnprintf(buffer, kBufferSize, format, args);
+    va_end(args);
+    fprintf(stderr, "%s", buffer);
+}
+
 static std::ostream& err() {
   return std::cerr << "error: ";
 }
@@ -107,8 +121,8 @@ int main(int argc, char* argv[]) {
         }
         break;
         default: {
-          err() << "unknown option \"" << word << "\"." << std::endl
-                << cdep_usage;
+          err() << "unknown option \"" << word << "\"." << std::endl;
+          SkDebugf(cdep_usage);
           return -1;
         }
         break;
