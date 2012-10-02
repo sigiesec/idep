460247752aef7858081a3ac6985811785228f4cf
Thiago Farina <tfarina@chromium.org>
2012-10-02 13:27:37 -0300

    Rename SkDebugf() to just Printf().
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- cdep.cc
+++ cdep.cc
@@ -1,5 +1,6 @@
 #include "idep_compile_dep.h"
 
+#include <stdarg.h>
 #include <stdio.h>
 
 #include <iostream>
@@ -24,18 +25,15 @@ const char cdep_usage[] =
 "\n"
 "    cdep -iincludes *.[ch]\n\n";
 
-static const size_t kBufferSize = 2048;
+const size_t kBufferSize = 2048;
 
-#include <stdarg.h>
-#include <stdio.h>
-
-void SkDebugf(const char format[], ...) {
-    char    buffer[kBufferSize + 1];
-    va_list args;
-    va_start(args, format);
-    vsnprintf(buffer, kBufferSize, format, args);
-    va_end(args);
-    fprintf(stderr, "%s", buffer);
+void Printf(const char format[], ...) {
+  char buffer[kBufferSize + 1];
+  va_list args;
+  va_start(args, format);
+  vsnprintf(buffer, kBufferSize, format, args);
+  va_end(args);
+  fprintf(stderr, "%s", buffer);
 }
 
 static std::ostream& err() {
@@ -121,8 +119,8 @@ int main(int argc, char* argv[]) {
         }
         break;
         default: {
-          err() << "unknown option \"" << word << "\"." << std::endl;
-          SkDebugf(cdep_usage);
+          Printf("error: unknown option \"%s\".\n", word);
+          Printf(cdep_usage);
           return -1;
         }
         break;
